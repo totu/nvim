1dbeacc1a5d4ae69e10444f63cbd40eef8c8d59b
Topi Tuulensuu <topi@topituulensuu.com>
2023-02-20 22:35:54 +0200

    try to convert to lua

---
+++ init.lua
@@ -0,0 +1,102 @@
+-- packer stuff
+require("plugins")
+
+-- binds
+vim.g.mapleader = " "
+-- because nvim devs suck (https://github.com/neovim/neovim/issues/416)
+vim.keymap.set("n", "Y", "Y")
+-- move line when visual
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+-- stay on same line when J-ing
+vim.keymap.set("n", "J", "mzJ`z")
+-- go back to last buffer
+vim.keymap.set("n", "<tab>", ":b#<cr>")
+-- delete current buffer
+vim.keymap.set("n", "<leader>d", ":bd<cr>")
+-- robot shit
+vim.keymap.set("i", "<C-space>", "<space><space><space><space>")
+--vim.keymap.set("i", "<S-space>", "<space><space><space><space>")
+vim.keymap.set("n", "<leader>j", ":%!python -m json.tool<CR>")
+vim.keymap.set("n", "<leader>r", ":%!python -m robot.tidy %:p<CR>")
+
+-- does this work over ssh?
+vim.keymap.set("n", "<leader>y", "\"+y<cr>")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- :sus:
+vim.keymap.set("n", "<leader>m", function()
+    vim.lsp.buf.format()
+end)
+
+
+-- line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+    callback = function()
+        vim.opt.relativenumber = false
+    end
+})
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    callback = function()
+        vim.opt.relativenumber = true
+    end
+})
+
+-- tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+-- swap sux
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+vim.opt.undofile = true
+
+-- search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+
+-- lol
+vim.cmd("abbrev spadde spade")
+vim.cmd("abbrev Commetn Comment")
+vim.cmd("abbrev Comemtn Comment")
+
+-- colors
+vim.opt.termguicolors = true
+function WTF(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+WTF()
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "100"
+
+-- remove whitespaces on lines I touched
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+        vim.cmd("EnableStripWhitespaceOnSave")
+    end
+})
+
+
+
+-- Bughunting and ops stuff, once you’ve gotten the hang of things.
+-- The only “new” info you ever learn about a production system is that it is broken or,
+-- even worse, that it is somehow managing to deliver value despite having the software equivalent
+-- of hyperdimensional aggressive bone cancer.
+-- Instead of merely not affording chances to learn or explore, this work actively punishes
+-- you for digging into the abyss.
---
+++ lua/plugins.lua
@@ -0,0 +1,64 @@
+-- Packer shit
+vim.cmd [[packadd packer.nvim]]
+return require("packer").startup(function(use)
+    use "wbthomason/packer.nvim"
+    use {
+        "nvim-telescope/telescope.nvim", tag = "0.1.1",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
+    use {
+        "morhetz/gruvbox",
+        as = "gruvbox",
+        config = function()
+            vim.cmd("colorscheme gruvbox")
+        end
+    }
+
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use "mbbill/undotree"
+    use "tpope/vim-fugitive"
+
+    -- zero LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            { 'williamboman/mason.nvim' }, -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
+
+    use "christoomey/vim-tmux-navigator"
+    use "ntpeters/vim-better-whitespace"
+
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    use "tpope/vim-commentary"
+
+    use "xiyaowong/nvim-transparent"
+
+end)
